defea0547316a45cdc1a9fdda9c602395eb76bcc
condekind <brunocondekind@hotmail.com>
2017-10-08 16:39:19 -0300

    vm.c

--- vm_BACKUP_6227.c
+++ vm_BACKUP_6227.c
@@ -344,6 +344,7 @@ copyuvm(pde_t *pgdir, uint sz)
 pde_t*
 copyuvmcow(pde_t *pgdir, uint sz)
 {
+<<<<<<< HEAD
 	pde_t *d;
 	pte_t *pte;
 	uint pa, i, flags;
@@ -373,6 +374,35 @@ copyuvmcow(pde_t *pgdir, uint sz)
 //bad:
 //  freevm(d);
 //  return 0;
+=======
+  pde_t *d;
+  pte_t *pte;
+  uint pa, i, flags;
+  char *mem;
+
+  lcr3(V2P(myproc()->pgdir));
+  
+  if((d = setupkvm()) == 0)
+    return 0;
+  for(i = 0; i < sz; i += PGSIZE){
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
+      goto bad;
+  }
+  return d;
+
+bad:
+  freevm(d);
+  return 0;
+>>>>>>> cf5b52a146e24d49623fb1feea4bffbeb4ef08bb
 }
 
 //PAGEBREAK!
